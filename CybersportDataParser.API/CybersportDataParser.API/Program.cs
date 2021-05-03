3785589c714cb29ee306531b261ee8385109a53c
Misha Bukhalo <bukhalo1999@gmail.com>
2021-05-03 11:10:26 +0300

    basic setup for application

---
+++ CybersportDataParser.API/CybersportDataParser.API/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace CybersportDataParser.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+            })
+            .UseStartup<Startup>();
+    }
+}
---
+++ CybersportDataParser.API/CybersportDataParser.ConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CybersportDataParser.ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
